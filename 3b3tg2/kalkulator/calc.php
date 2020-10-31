71bcc1126e95b38557e2d8c1db5a131910433a15
zziembiewicz <70716919+zziembiewicz@users.noreply.github.com>
2020-10-31 20:14:59 +0100

    Add files via upload

--- 3b3tg2/kalkulator/calc.php
+++ 3b3tg2/kalkulator/calc.php
@@ -22,6 +22,33 @@ table{
 
       require_once './function.php';
 
+      if(isset($_POST['submit']))
+    	{
+      if(is_numeric($_POST['x']) && is_numeric($_POST['y']))
+     {
+       $x = $_POST['x'];
+       $y = $_POST['y'];
+     }
+     if (isset($_POST["sign"])){
+            $option=$_POST["sign"];
+             switch ($option){
+               case 'sum':
+               $result=sum($x,$y);
+               break;
+               case 'sub':
+               $result=sub($x,$y);
+               break;
+               case 'mul':
+               $result=mul($x,$y);
+               break;
+               case 'div':
+               $result=div($x,$y);
+               break;
+               default:
+               $result="select operation";
+             }
+           }
+      }
   ?>
 
     <table>
@@ -38,8 +65,8 @@ table{
     </select>
     </td>
     <td><input type="number" name="y" value="y" placeholder="y" pattern="[0-9]"></td>
-    <td><input type="submit" value="="> </td>
-    <td><input type="number" name="score" value="<?php echo($result); ?>" disabled></td>
+    <td><input type="submit" name="submit" value="="> </td>
+    <td><input type="text" name="result" disabled value= "<?php if(isset ($result)){echo $result;}?>"></td>
 
     <tr>
     </form>
--- 3b3tg2/kalkulator/function.php
+++ 3b3tg2/kalkulator/function.php
@@ -1,49 +1,24 @@
 <?php
+function sum($x, $y){
+$result = $x + $y;
+}
+function sub($x, $y){
+$result = $x - $y;
+}
 
-$x=isset($_POST['x']);
-$y=isset($_POST['y']);
+function mul($x, $y){
+$result = $x * $y;
+}
 
-if (isset($_POST["option"])){
-   switch ($option){
-        case'sum':
-            function sum($x, $y):string{
-                $result = $x + $y;
-                return $result;
-              }
-        break;
-
-        case'min':
-            function min($x, $y):string{
-                $result = $x - $y;
-                return $result;
-              }
-        break;
-
-        case'mul':
-            function mul($x, $y):string{
-                $result = $x * $y;
-                return $result;
-              }
-        break;
-
-        case'div':
-            function div($x, $y):string{
-              if(y==0)
-              {
-                echo "Do not divide by 0";
-              }
-              else
-              {
-                $result = $x / $y;
-                return $result;
-
-              }
-            }
-        break;
-
-        default:
-          echo "command not found";
-    }
+function div($x, $y){
+  if(y==0)
+  {
+  echo "Do not divide by 0";
+  }
+  else
+  {
+  $result = $x / $y;
+  }
 }
 
 ?>
