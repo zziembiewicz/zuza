213c0ae343eb53b2a108a707afdfbadb2373ef7d
zziembiewicz <70716919+zziembiewicz@users.noreply.github.com>
2020-11-08 16:25:54 +0100

    Add files via upload

--- 3b3tg2/5.11.2020/ostroslup.php
+++ 3b3tg2/5.11.2020/ostroslup.php
@@ -38,14 +38,44 @@ hr{
     <td><input type="number" name="a"  placeholder="a" pattern="[0-9]"></td>
     </tr>
     <tr>
-    <td><input type="number" name="b"  placeholder="b" pattern="[0-9]"></td>
-    </tr>
-    <tr>
-    <td><input type="number" name="c"  placeholder="c" pattern="[0-9]"></td>
+    <td><input type="number" name="h"  placeholder="h" pattern="[0-9]"></td>
     </tr>
     <tr>
     <td><input type="submit" name="oblicz" value="Oblicz"></td>
     </tr>
+    <td>
+    <?php
+    echo ost()
+    ?>
+    </td>
+    </tr>
+    <tr>
+    <td><a href="3_zadanie_formularz_geometria.php">Powrót do strony głównej</a></td>
+    </tr>
+    </form>
     </table>
-</body>
-</html>
+    <?php
+
+    function ost(){
+    if (isset($_POST['a'],$_POST['h'])){
+         if($_POST['a']<=0||$_POST['h']<=0){
+            echo "<br><h4>Liczby podane w polach powinny być dodatnie</h4>";
+        }
+        else{
+            require_once('./scripts/ostroslup.php');
+
+
+        echo '<ul>';
+        echo '<li>Pole:'.P_Ostro($a,$b,$c).'cm<sup>2</sup></li>';
+        echo '<li>Objętość:'.O_Ostro($a,$b,$c).'cm<sup>3</sup></li>';
+        echo '</ul>';
+
+
+        }
+    }
+
+    }
+
+    ?>
+    </body>
+    </html>
--- 3b3tg2/5.11.2020/scripts/ostroslup.php
+++ 3b3tg2/5.11.2020/scripts/ostroslup.php
@@ -1,7 +1,11 @@
 <?php
-function Pole_Ostro($a,$h){
+if (isset($_POST['a'], $_POST['h'])){
+    $a=$_POST['a'];
+    $b=$_POST['h'];
+    }
+function P_Ostro($a,$h){
 	return number_format(round((pow($a,2)+3*$a*$h),2),2,'.','');
 }
-function Obje_Ostro($a,$h){
+function O_Ostro($a,$h){
 	return number_format(round((pow($a,2)*$h/3),2),2,'.','');
 } ?>
